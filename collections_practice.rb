a74393856ca7b830c3bac864fc39baf52510f247
Scarlett Perry <si.perry15@gmail.com>
2018-07-15 15:11:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
 # your code goes here
+
+def begins_with_r (array)
+  array.all? do |word|
+    word.start_with?("r")
+  end
+end
+
+def contain_a (array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa (array)
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings (array)
+  array.delete_if do |word|
+    !word.kind_of?(String)
+  end
+end
+
+def count_elements (array)
+count = Hash.new(0)
+  array.each do |word|
+    count[word] +=1
+  end
+count
+end
+
+def merge_data(hash_keys, hash_data)
+new_keys = hash_keys.keys
+new_data = hash_data.values
+new_hash = []
+
+new_hash[new_keys][new_data]
+end 
